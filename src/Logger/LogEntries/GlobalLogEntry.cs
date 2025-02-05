092b567691c3dfa4c5898b13cd70fccfc20d0429
Florent <florent.maillard-salin@jurihub.fr>
2025-02-05 20:57:18 +0100

    feat: Fix GlobalLogEntry logics

--- src/Logger/LogEntries/GlobalLogEntry.cs
+++ src/Logger/LogEntries/GlobalLogEntry.cs
@@ -3,14 +3,18 @@ using System.Text.Json.Serialization;
 
 namespace Logger.LogEntries;
 
-public class GlobalLogEntry : ILogEntry
+public class GlobalLogEntry(
+    string loggedOperation, 
+    string message, 
+    Dictionary<string, object>? metadata = null)
+    : ILogEntry
 {
     public DateTime Timestamp { get; } = DateTime.UtcNow;
-    public string LoggedOperation { get; set; }
-    public required string Message { get; set; }
-    
+    public string LoggedOperation { get; set; } = loggedOperation;
+    public string Message { get; set; } = message;
+
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-    public Dictionary<string, object>? Metadata { get; set; }
+    public Dictionary<string, object>? Metadata { get; set; } = metadata;
 
     public override string ToString() => $"[{Timestamp}] - {LoggedOperation} - {Message}";
     public string ToJson() => JsonSerializer.Serialize(this, new JsonSerializerOptions { WriteIndented = true });
