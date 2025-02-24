3c419ea09ace0dcd7691a73ff6ed3b074f64ff3d
Florent <florent.maillardsalin@viacesi.fr>
2025-02-24 14:14:42 +0100

    feat: move logtransporters in settings

--- src/EasySave/Services/SettingsService.cs
+++ src/EasySave/Services/SettingsService.cs
@@ -26,10 +26,8 @@ public static class SettingsService
         EnsureSettingsExists();
         
         var fullSettingsPath = Path.GetFullPath(SettingsFile);
-        Watcher = new FileSystemWatcher(Path.GetDirectoryName(fullSettingsPath) ?? string.Empty)
-        {
-            Filter = Path.GetFileName(fullSettingsPath), NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.Size
-        };
+        Watcher = new FileSystemWatcher(Path.GetDirectoryName(fullSettingsPath) ?? string.Empty) 
+            { Filter = Path.GetFileName(fullSettingsPath), NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.Size };
         Watcher.Changed += OnSettingsFileChanged;
         Watcher.EnableRaisingEvents = true;
     }
@@ -74,6 +72,7 @@ public static class SettingsService
         Watcher.EnableRaisingEvents = false;
         FileHelper.CreateAndWrite(SettingsFile, jsonSettings);
         Watcher.EnableRaisingEvents = true;
+        
     }
     
     public static void UpdateLanguage(String languageCode)
