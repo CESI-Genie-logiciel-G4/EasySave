3ed41e5aa869569db2dfd173774774924045d880
Brieuc Olv <brieuc.olivieri@gmail.com>
2025-02-25 17:12:54 +0100

    test: enhance MenuItem constructor tests and add action execution verification

--- tests/EasySave.Tests/Utils/MenuItemTests.cs
+++ tests/EasySave.Tests/Utils/MenuItemTests.cs
@@ -4,14 +4,48 @@ namespace EasySave.Tests.Utils;
 
 public class MenuItemTests
 {
-    [Fact] 
-    public void MenuItem_WhenCreatedWithAction_ShouldHaveAction()
+    [Fact]
+    public void Constructor_WithTitleAndFuncAction_ShouldSetProperties()
     {
-        var action = new Action(() => { });
-        const string title = "Title";
-        
+        // Arrange
+        string title = "Test Title";
+        Func<Task> action = () => Task.CompletedTask;
+
+        // Act
         var menuItem = new MenuItem(title, action);
-        
+
+        // Assert
+        Assert.Equal(title, menuItem.Title);
         Assert.Equal(action, menuItem.Action);
+    }
+
+    [Fact]
+    public void Constructor_WithTitleAndAction_ShouldSetProperties()
+    {
+        // Arrange
+        string title = "Test Title";
+        Action action = () => { };
+
+        // Act
+        var menuItem = new MenuItem(title, action);
+
+        // Assert
+        Assert.Equal(title, menuItem.Title);
+        Assert.NotNull(menuItem.Action);
+    }
+
+    [Fact]
+    public async Task Action_ShouldExecuteProvidedAction()
+    {
+        // Arrange
+        var actionExecuted = false;
+        var action = () => { actionExecuted = true; };
+        var menuItem = new MenuItem("Test Title", action);
+
+        // Act
+        await menuItem.Action();
+
+        // Assert
+        Assert.True(actionExecuted);
     }
 }
