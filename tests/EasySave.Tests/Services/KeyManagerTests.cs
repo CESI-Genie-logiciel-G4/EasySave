97ece29c8e2be94e5726900378f855ec2d99ab01
Brieuc Olv <brieuc.olivieri@gmail.com>
2025-02-26 10:25:10 +0100

    test: Sonar issue

--- tests/EasySave.Tests/Services/KeyManagerTests.cs
+++ tests/EasySave.Tests/Services/KeyManagerTests.cs
@@ -2,65 +2,94 @@ using EasySave.Services;
 
 namespace EasySave.Tests.Services;
 
-public class KeyManagerTests : IDisposable
+public sealed class KeyManagerTests : IDisposable
+{
+    private const string KeyPath = ".easysave/security/encryption_key";
+    private const string IvPath = ".easysave/security/encryption_iv";
+    private bool _disposed;
+
+    public KeyManagerTests()
     {
-        private const string KeyPath = ".easysave/security/encryption_key";
-        private const string IvPath = ".easysave/security/encryption_iv";
+        Cleanup();
+    }
 
-        public KeyManagerTests()
+    [Fact]
+    public void GetKey_ShouldReturnSameKeyBetweenCalls()
+    {
+        // Arrange
+        var keyManager = KeyManager.GetInstance();
+
+        // Act
+        byte[] key1 = keyManager.GetKey();
+        byte[] key2 = keyManager.GetKey();
+
+        // Assert
+        Assert.Equal(key1, key2);
+    }
+
+    [Fact]
+    public void GetIv_ShouldReturnSameIvBetweenCalls()
+    {
+        // Arrange
+        var keyManager = KeyManager.GetInstance();
+
+        // Act
+        byte[] iv1 = keyManager.GetIv();
+        byte[] iv2 = keyManager.GetIv();
+
+        // Assert
+        Assert.Equal(iv1, iv2);
+    }
+
+    [Fact]
+    public void GetKey_ShouldReturnValidKey()
+    {
+        // Act
+        var keyManager = KeyManager.GetInstance();
+        byte[] key = keyManager.GetKey();
+
+        // Assert
+        Assert.NotNull(key);
+        Assert.True(File.Exists(KeyPath));
+    }
+
+    [Fact]
+    public void GetInstance_ShouldReturnSameInstance()
+    {
+        // Act
+        var instance1 = KeyManager.GetInstance();
+        var instance2 = KeyManager.GetInstance();
+
+        // Assert
+        Assert.Same(instance1, instance2);
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    private void Dispose(bool disposing)
+    {
+        if (!_disposed)
         {
-            Cleanup();
-        }
-        
-        [Fact]
-        public void GetIv_ShouldReturnValidIv()
-        {
-            // Act
-            var keyManager = KeyManager.GetInstance();
-            byte[] iv = keyManager.GetIv();
-
-            // Assert
-            Assert.NotNull(iv);
-            Assert.True(File.Exists(IvPath));
-        }
-
-        [Fact]
-        public void GetKey_ShouldReturnSameKeyBetweenCalls()
-        {
-            // Arrange
-            var keyManager = KeyManager.GetInstance();
-
-            // Act
-            byte[] key1 = keyManager.GetKey();
-            byte[] key2 = keyManager.GetKey();
-
-            // Assert
-            Assert.Equal(key1, key2);
-        }
-
-        [Fact]
-        public void GetIv_ShouldReturnSameIvBetweenCalls()
-        {
-            // Arrange
-            var keyManager = KeyManager.GetInstance();
-
-            // Act
-            byte[] iv1 = keyManager.GetIv();
-            byte[] iv2 = keyManager.GetIv();
-
-            // Assert
-            Assert.Equal(iv1, iv2);
-        }
-
-        public void Dispose()
-        {
-            Cleanup();
-            GC.SuppressFinalize(this);
-        }
-
-        private static void Cleanup()
-        {
-            if (File.Exists(KeyPath)) File.Delete(KeyPath);
-            if (File.Exists(IvPath)) File.Delete(IvPath);
+            if (disposing)
+            {
+                Cleanup();
+            }
+            _disposed = true;
         }
     }
+
+    ~KeyManagerTests()
+    {
+        Dispose(false);
+    }
+
+    private static void Cleanup()
+    {
+        if (File.Exists(KeyPath)) File.Delete(KeyPath);
+        if (File.Exists(IvPath)) File.Delete(IvPath);
+    }
+}
